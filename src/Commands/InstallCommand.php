b132063d9c8fd8e8622499dd5417b31af4753d7a
taba2177 <tabadev217@gmail.com>
2025-07-23 16:58:39 +0300

    #

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -28,8 +28,8 @@ class InstallCommand extends Command
         Artisan::call('vendor:publish', ['--tag' => 'filament-peek-assets', '--force' => true,'--no-interaction' => true]);
         Artisan::call('curator:install',['--no-interaction' => true]);
 
-        // $this->comment('Publishing breezy dependencies...');
-        // Artisan::call('breezy:install',['--no-interaction' => true]);
+        $this->comment('Publishing breezy dependencies...');
+        Artisan::call('breezy:install',['--no-interaction' => true]);
 
         // Publish this package's configuration file.
         $this->comment('Publishing package configuration...');
