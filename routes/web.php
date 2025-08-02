7c7a7eb1959a88bc0cf63f55952055aec18d6f90
taba2177 <tabadev217@gmail.com>
2025-08-02 17:20:08 +0300

    #

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Spatie\Sitemap\Tags\Url;
 use Taba\Crm\Http\Controllers\GoogleTranslateController;
 use Taba\Crm\Livewire\Post\Show as PostShow;
 use Taba\Crm\Livewire\Posts;
-use Taba\Crm\livewire\Home;
+use Taba\Crm\Livewire\Home;
 use Taba\Crm\Http\Controllers\PageController;
 use Taba\Crm\Http\Controllers\PostController;
 use Taba\Crm\Http\Controllers\PreviewController;
@@ -15,7 +15,7 @@ use Taba\Crm\Models\Page;
 use Taba\Crm\Models\Post;
 use Taba\Crm\Models\PostCategory;
 
-// Route::middleware('web')->group(function () {
+Route::middleware('web')->group(function () {
 
 Route::get('/preview/post/{post:slug}', [PreviewController::class, 'post'])->name('preview.post');
 Route::get('/preview/category/{category:slug}', [CategoryPreviewController::class, 'category'])->name('preview.category');
@@ -50,7 +50,7 @@ Route::get('/{slug}', function ($slug) {
 Route::get('/{category}/{post:slug}', [PostController::class, 'show'])
     ->name('posts.show');
 
-// });
+});
 
 Route::get('/sitemap', function () {
     // Create sitemap instance
