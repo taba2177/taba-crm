9cdf33c167d31c5d33067fd2872dc023d8419d98
taba2177 <tabadev217@gmail.com>
2025-09-11 15:10:58 +0300

    #

--- src/Models/User.php
+++ src/Models/User.php
@@ -67,6 +67,7 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
     public function canAccessPanel(Panel $panel): bool
     {
         return $this->hasRole('super_admin') || $this->hasRole('admin') || $this->hasRole('client');
+        return true;
     }
 
     /**
