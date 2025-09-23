c559fce9279bfe6d4213a4adc1b99e642b2ebafe
taba2177 <tabadev217@gmail.com>
2025-09-23 22:16:23 +0300

    #

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\App\Crmlication;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
+use Taba\Crm\Exceptions\Handler;
 
 return Application::configure(basePath: dirname(__DIR__))
 
@@ -30,9 +30,15 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->redirectTo(fn () => Filament\Pages\Dashboard::getUrl());
     })
     ->withExceptions(function (Exceptions $exceptions) {
-    //       $exceptions->renderable(function (NotFoundHttpException $e, $request) {
-    //     return redirect('/', 302); // Use 301 for permanent redirect
-    // });
+        
+        $exceptions->handler(function ($e) {
+            return app(Handler::class)->render(request(), $e);
+        });
+
+        // You can also report exceptions using your handler if needed
+        $exceptions->report(function (Throwable $e) {
+            app(Handler::class)->report($e);
+        });
         $exceptions->reportable(fn (Throwable $e) => $exceptions->handler->shouldReport($e) &&
             FilamentExceptions::report($e)
         );
