cab250633361e91393a79d34114a36e89d4cc5ae
taba2177 <tabadev217@gmail.com>
2025-09-25 15:01:52 +0300

    #

--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -113,7 +113,7 @@ class PostController extends Controller
             $Schema = Schema::service()
                 ->name($this->post->title)
                 // Safely get description: check meta_description, then excerpt, then content
-                ->description($this->post->meta_description ?: $this->post->blocks[0]->data->content ?? $this->post->title)
+                ->description($this->post->meta_description ?: $this->post->blocks[0]?->data->content ?? $this->post->title)
                 ->url($this->post->url)
                 ->image($this->post->image?->url)
                 ->provider(
@@ -129,7 +129,7 @@ class PostController extends Controller
 
             $Schema = Schema::article()
                 ->headline($this->post->title)
-                ->articleBody($this->post->meta_description ?: $this->post->blocks[0]->data->content)
+                ->articleBody($this->post->meta_description ?: $this->post->blocks[0]?->data->content ?? $this->post->title)
                 ->image($this->post->image?->url)
                 ->datePublished($this->post->published_at)
                 ->dateModified($this->post->updated_at)
