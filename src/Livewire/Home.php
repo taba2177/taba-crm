484cf8ff4e4a560e87f2677e8285bf028f3c4853
taba2177 <tabadev217@gmail.com>
2025-09-25 13:05:22 +0300

    #

--- src/Livewire/Home.php
+++ src/Livewire/Home.php
@@ -97,12 +97,12 @@ class Home extends Component
     // NOTE: I've made your SEO methods more robust to handle cases where data might not be loaded yet.
     public function prepareInitialSeoData()
     {
-        $seoPost = Post::where("show_in_home", true)
-            ->published()
-            ->select('title', 'meta_title', 'meta_description', 'content', 'image_id') // Use `content` as fallback
-            ->latest()
+        $seoPost = PostCategory::whereNotNull('section_component')
+            ->withCount(['posts' => function ($query) {
+                $query->where("show_in_home", true)->published()->latest()->orderBy('order','asc')->first();
+            }])
             ->orderBy('order','asc')
-            ->first();
+            ->posts?->first();
 
         if ($seoPost) {
             $this->seoimage = $seoPost->image?->url;
