d2333665ae092caebb19c4eead9a86a1d9ed0018
taba2177 <tabadev217@gmail.com>
2025-11-18 12:22:05 +0300

    #

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -24,12 +24,13 @@ class InstallCommand extends Command
         // Run installation tasks
         if (!$this->task('Running dependency installers', fn() => $this->installDependencies())) return self::FAILURE;
         if (!$this->task('Running database migrations', fn() => $this->runMigrations())) return self::FAILURE;
-        if (!$this->task('Installing NPM packages', fn() => $this->runNpmInstall())) return self::FAILURE;
-        if (!$this->task('Publishing package assets', fn() => $this->publishAssets())) return self::FAILURE;
+        // Update package.json before running npm so new devDependencies are installed.
         if (!$this->task('Updating package.json', fn() => $this->updateNodeDependencies())) return self::FAILURE;
         if (!$this->task('Configuring Tailwind CSS', fn() => $this->updateTailwindConfig())) return self::FAILURE;
         if (!$this->task('Configuring Vite', fn() => $this->updateViteConfig())) return self::FAILURE;
         if (!$this->task('Ensuring PostCSS is configured', fn() => $this->updatePostCssConfig())) return self::FAILURE;
+        if (!$this->task('Installing NPM packages', fn() => $this->runNpmInstall())) return self::FAILURE;
+        if (!$this->task('Publishing package assets', fn() => $this->publishAssets())) return self::FAILURE;
         if (!$this->task('Building frontend assets', fn() => $this->runNpmBuild())) return self::FAILURE;
 
         if ($this->option('publish-views')) {
@@ -174,7 +175,7 @@ class InstallCommand extends Command
         $stub = <<<'EOT'
 module.exports = {
     plugins: {
-        "tailwindcss/nesting": {},
+        'postcss-nesting': {},
         tailwindcss: {},
         autoprefixer: {},
     },
