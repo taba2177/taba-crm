54b993c443f8084452c19b85b3880b7074d63938
taba2177 <tabadev217@gmail.com>
2025-11-18 16:56:25 +0300

    #

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -13,57 +13,57 @@ class RolesAndPermissionsSeeder extends Seeder
         // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // create permissions
-        // create permissions for articles (existing)
-        Permission::create(['name' => 'edit articles']);
-        Permission::create(['name' => 'delete articles']);
-        Permission::create(['name' => 'publish articles']);
-        Permission::create(['name' => 'unpublish articles']);
+        // Define all permissions
+        $permissions = [
+            'edit articles',
+            'delete articles',
+            'publish articles',
+            'unpublish articles',
+            'create posts',
+            'edit posts',
+            'delete posts',
+            'create categories',
+            'edit categories',
+            'delete categories',
+            'view component_section',
+            'view ai_tools',
+            'manage users',
+        ];
 
-        // create permissions for posts
-        Permission::create(['name' => 'create posts']);
-        Permission::create(['name' => 'edit posts']);
-        Permission::create(['name' => 'delete posts']);
+        // Create permissions only if they don't exist
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission, 'guard_name' => 'web']);
+        }
 
-        // create permissions for categories
-        Permission::create(['name' => 'create categories']);
-        Permission::create(['name' => 'edit categories']);
-        Permission::create(['name' => 'delete categories']);
+        // Create roles only if they don't exist
+        $adminRole = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        $adminRole->syncPermissions([
+            'edit articles',
+            'delete articles',
+            'publish articles',
+            'unpublish articles',
+            'create posts',
+            'edit posts',
+            'delete posts',
+            'create categories',
+            'edit categories',
+            'delete categories',
+            'view component_section',
+            'view ai_tools',
+            'manage users',
+        ]);
 
-        // create permissions for specific features
-        Permission::create(['name' => 'view component_section']);
-        Permission::create(['name' => 'view ai_tools']);
-        Permission::create(['name' => 'manage users']);
+        $clientRole = Role::firstOrCreate(['name' => 'client', 'guard_name' => 'web']);
+        $clientRole->syncPermissions([
+            'create posts',
+            'edit posts',
+            'delete posts',
+            'create categories',
+            'edit categories',
+            'delete categories',
+        ]);
 
-        // create roles and assign created permissions
-        $adminRole = Role::create(['name' => 'admin'])
-            ->givePermissionTo([
-                'edit articles',
-                'delete articles',
-                'publish articles',
-                'unpublish articles',
-                'create posts',
-                'edit posts',
-                'delete posts',
-                'create categories',
-                'edit categories',
-                'delete categories',
-                'view component_section',
-                'view ai_tools',
-                'manage users',
-            ]);
-
-        $clientRole = Role::create(['name' => 'client'])
-            ->givePermissionTo([
-                'create posts',
-                'edit posts',
-                'delete posts',
-                'create categories',
-                'edit categories',
-                'delete categories',
-            ]);
-
-        $superAdminRole = Role::create(['name' => 'super-admin']);
-        $superAdminRole->givePermissionTo(Permission::all());
+        $superAdminRole = Role::firstOrCreate(['name' => 'super-admin', 'guard_name' => 'web']);
+        $superAdminRole->syncPermissions(Permission::all());
     }
 }
