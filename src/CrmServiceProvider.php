993e585786c98f9966949439d6b7316891549948
taba2177 <tabadev217@gmail.com>
2025-11-19 15:26:01 +0300

    Fix: Simplify view publishing path in CrmServiceProvider

--- src/CrmServiceProvider.php
+++ src/CrmServiceProvider.php
@@ -63,15 +63,7 @@ class CrmServiceProvider extends ServiceProvider
             ], 'crm-config');
 
             $this->publishes([
-                __DIR__.'/views' => resource_path('views/vendor/crm'),
-                __DIR__.'/views/components/logo.blade.php' => resource_path('views/components/logo.blade.php'),
-                __DIR__.'/views/components/homepage/four-cards.blade.php' => resource_path('views/components/homepage/four-cards.blade.php'),
-                __DIR__.'/views/components/layouts' => resource_path('views/components/layouts'),
-                __DIR__.'/views/components/templates' => resource_path('views/components/templates'),
-                __DIR__.'/views/livewire' => resource_path('views/livewire'),
-                __DIR__.'/views/posts' => resource_path('views/posts'),
-                __DIR__.'/views/previews' => resource_path('views/previews'),
-                __DIR__.'/views/filament/pages' => resource_path('views/filament/pages'),
+                __DIR__.'/../views' => resource_path('views/vendor/crm'),
             ], 'crm-views');
 
             // Publish public assets only if the directory exists to avoid errors
